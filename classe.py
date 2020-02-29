76f683fec26a2018b531ebd359447df1173953d1
Yacine Guerandi <61632972+yacinegrd@users.noreply.github.com>
2020-02-29 20:16:44 +0100

    Add files via upload

---
+++ classe.py
@@ -0,0 +1,153 @@
+#coding:utf-8
+from random import randrange
+import pygame
+
+#-- ClASS --------------------------------------------------------------#
+class snake(object):
+
+	vel = 17
+	w = vel-1
+	h = vel-1
+	lastmove = "right"
+
+	def __init__(self):
+		x = snake.vel*4 + 1
+		y = snake.vel*10 + 1
+	
+		head = pygame.Rect( x  , y , snake.w , snake.h)
+		cube2 = pygame.Rect( x - 1 * snake.vel , y , snake.w , snake.h)
+
+		self.body = [ head , cube2 ]
+
+	def MoveSnake( self ) :
+		
+		keys = pygame.key.get_pressed() 	
+
+	#-- manuelle --------------------------------------- 
+		if keys[pygame.K_UP] and self.lastmove != "down"  :
+			self.body[0].y -= snake.vel
+			self.lastmove = "up"
+		elif keys[pygame.K_DOWN] and self.lastmove !="up":
+			self.body[0].y += snake.vel
+			self.lastmove = "down"
+		elif keys[pygame.K_RIGHT] and self.lastmove !="left":
+			self.body[0].x += snake.vel
+			self.lastmove = "right"
+		elif keys[pygame.K_LEFT] and self.lastmove !="right" :
+			self.body[0].x -= snake.vel
+			self.lastmove = "left"	
+	#-- auto --------------------------------------------
+		elif self.lastmove == "up" :
+			self.body[0].y -= snake.vel
+		elif self.lastmove == "down" :
+			self.body[0].y += snake.vel
+		elif self.lastmove == "right" :
+			self.body[0].x += snake.vel
+		elif self.lastmove == "left" :
+			self.body[0].x -= snake.vel
+
+	def	DrawSnake(self, surface):
+		black = (0 , 0 , 0)
+		for i in range(0 ,len(self.body)) :
+			pygame.draw.rect(surface , black , self.body[i] )
+		
+		x = len(self.body) - 2
+		while x >= 0 :
+			self.body[x+1].x = self.body[x].x
+			self.body[x+1].y = self.body[x].y
+			x -= 1   
+	
+	def Grow(self):
+		self.body.append( pygame.Rect( self.body[len(self.body) - 1] ) )
+
+	def Die(self , screen_dim):
+		
+		die = False
+
+		for i in range( 1 , len(self.body) ) :
+			x_distence = self.body[0].x - self.body[i].x
+			y_distence = self.body[0].y - self.body[i].y
+
+			if abs(x_distence) < 1  and abs(y_distence) < 1 :
+				die = True
+			pass
+
+		screen_width = screen_dim[0] 
+		screen_hight = screen_dim[1]
+			
+		if self.body[0].x > screen_width - snake.w  :
+			die = True		
+		elif self.body[0].y > screen_hight - snake.h : 
+			die = True
+		elif self.body[0].x < 0 :
+			die = True
+		elif self.body[0].y < 31 :
+			die = True
+
+		return die
+
+class food(object):
+
+	def __init__(self , surface_dim , vel):
+
+		w = vel-1
+		h = w 
+
+		x_food = vel*randrange( 0 , int(surface_dim[0]/vel) ) + 1
+		y_food = vel*randrange( 2 , int(surface_dim[1]/vel) ) + 1
+		
+		self.food_cube = pygame.Rect( x_food , y_food , w , h )
+
+	def DrawFood(self , surface):		
+		green = (255 , 0 , 0)
+		pygame.draw.rect(surface , green , self.food_cube )
+
+	def Eaten(self , snake) :
+		x_distence = snake.body[0].x - self.food_cube.x
+		y_distence = snake.body[0].y - self.food_cube.y
+		
+		if  abs(x_distence) < 1 and  abs(y_distence) < 1 :
+			return True
+
+		else :
+			return False
+	
+	def NewFood(self, surface_dim , snake):
+
+		self.food_cube[0] = snake.vel*randrange( 0 , int(surface_dim[0]/snake.vel) ) + 1
+		self.food_cube[1] = snake.vel*randrange( 2 , int(surface_dim[1]/snake.vel) ) + 1
+
+		for i in range( 0 , len(snake.body) ) :
+			x_distence = self.food_cube.x - snake.body[i].x
+			y_distence = self.food_cube.y - snake.body[i].y
+
+			if abs(x_distence) < 1  and abs(y_distence) < 1 :
+				self.NewFood(surface_dim , snake)
+
+#-- FONCTION ----------------------------------------------------------------#	
+def WriteScore(screen , text , i ):
+	black = (0,0,0)
+	pos = (10, 2)
+	score = text.render("score : {}".format(i) , True , black )
+	screen.blit( score , pos )
+	pygame.draw.line(screen , black , (0 , 30) , (600 , 30) , 2 )
+
+def YouLost(screen , text , icon , score):	
+	black = (0,0,0)
+	
+	x_pos = screen[0]
+	y_pos = screen[1]
+
+	pos = (220, 280)
+	screen.fill((204,204,204))
+	Lost_message = text.render("You Lost !" , True , black )
+	screen.blit( Lost_message , pos )
+	screen.blit(icon , (280 , 320))
+	WriteScore(screen , text , score)
+ 
+def MouseClic( xicon , yicon):
+	if pygame.mouse.get_pressed()[0]:
+		if xicon < pygame.mouse.get_pos()[0] < xicon + 30 and yicon < pygame.mouse.get_pos()[1] <  yicon + 30 :
+			return True
+	else : 
+		return False 
---
+++ main.py
@@ -0,0 +1,53 @@
+#coding:utf-8
+import pygame 
+from classe import *
+#-- Declaration -----------------------------------------------------------#
+Run = True
+score = 0
+xicon = 280
+yicon = 320
+lose = False
+mysnake = snake()
+resolution = ( mysnake.vel*30 , mysnake.vel*30+34 )
+myfood = food(resolution, mysnake.vel)
+#-- Creat window ----------------------------------------------------------#
+pygame.init()
+screen = pygame.display.set_mode(resolution)
+pygame.display.set_caption("Snake")
+text = pygame.font.Font("3Dventure.ttf" , 30 )
+retry_icon = pygame.image.load("retry.png")
+retry_icon.convert_alpha()
+
+while Run :
+#-- EXIT ------------------------------------------------------------------#
+	pygame.time.delay(100)
+	for event in pygame.event.get() :
+		if event.type == pygame.QUIT :
+			Run = False
+#-- Restart Game ----------------------------------------------------------#
+	if mysnake.Die(resolution) or lose :
+		YouLost(screen , text, retry_icon , score)
+		lose = True
+		if MouseClic( xicon , yicon):
+			mysnake = snake()
+			myfood = food(resolution, mysnake.vel )
+			score = 0
+			lose = False
+#-- DRAW -------------------------------------------------------------------#	
+	if not lose	:
+		screen.fill((204,204,204))
+		myfood.DrawFood(screen)
+		mysnake.DrawSnake(screen)
+		WriteScore(screen , text , score)	
+#-- MOVE -------------------------------------------------------------------#
+	
+	mysnake.MoveSnake()
+	
+	if myfood.Eaten(mysnake):
+		myfood.NewFood(resolution , mysnake)
+		mysnake.Grow()
+		score += 1
+
+	pygame.display.update()
+#-- End --------------------------------------------------------------------#
+pygame.quit()
